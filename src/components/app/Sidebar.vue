b88d22e13aa3fafbae22267b99dc190f0f084c59
i.dyutkin <ilia.diutkin@stm-labs.ru>
2021-10-19 14:04:30 +0300

    add routing

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: this.isOpen }">
+    <router-link
+      v-for="(linkitem, index) in links"
+      :key="index"
+      tag="li"
+      active-class="active"
+      :to="linkitem.url"
+      :exact="linkitem.exact"
+    >
+      <a href="linkitem.url" class="waves-effect waves-orange pointer">{{
+        linkitem.title
+      }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { title: "Счет", url: "/", exact: true },
+        { title: "История", url: "/history", exact: false },
+        { title: "Планирование", url: "/planning", exact: false },
+        { title: "Новая запись", url: "/record", exact: false },
+        { title: "Категории", url: "/categories", exact: false },
+      ],
+    };
+  },
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -12,15 +12,64 @@ const routes = [
   },
   {
     path: "/login",
-    name: "login",
+    name: "Login",
     meta: {
       layout: "empty",
     },
     component: () => import("../views/Login.vue"),
   },
   {
+    path: "/register",
+    name: "Register",
+    meta: {
+      layout: "empty",
+    },
+    component: () => import("../views/Register.vue"),
+  },
+  {
+    path: "/record",
+    name: "Record",
+    meta: {
+      layout: "main",
+    },
+    component: () => import("../views/Record.vue"),
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    meta: {
+      layout: "main",
+    },
+    component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "/planning",
+    name: "Planning",
+    meta: {
+      layout: "main",
+    },
+    component: () => import("../views/Planning.vue"),
+  },
+  {
+    path: "/history",
+    name: "History",
+    meta: {
+      layout: "main",
+    },
+    component: () => import("../views/History.vue"),
+  },
+  {
+    path: "/detail-record",
+    name: "DetailRecord",
+    meta: {
+      layout: "main",
+    },
+
+    component: () => import("../views/DetailRecord.vue"),
+  },
+  {
     path: "/categories",
-    name: "categories",
+    name: "Categories",
     meta: {
       layout: "main",
     },
