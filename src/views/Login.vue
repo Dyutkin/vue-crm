c6226bea993698fff4a13450d55d9392da62a197
Мишагин Андрей Михайлович <ammisha1@mts.ru>
2021-10-22 16:52:56 +0300

    validation less-5

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,54 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{
+            ...(isMailNotValid
+              ? { invalid: true, validate: false }
+              : { invalid: false, validate: true }),
+          }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          v-if="this.$v.email.$dirty && !this.$v.email.required"
+          class="helper-text invalid"
+          >введите Email</small
+        >
+        <small
+          v-if="this.$v.email.$dirty && !this.$v.email.email"
+          class="helper-text invalid"
+          >Email введен в некорректном формате</small
+        >
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          type="password"
+          v-model="password"
+          :class="{
+            ...(isPasswordNotValid
+              ? { invalid: true, validate: false }
+              : { invalid: false, validate: true }),
+          }"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          v-if="this.$v.password.$dirty && !this.$v.password.required"
+          class="helper-text invalid"
+          >введите Password</small
+        >
+        <small
+          v-if="this.$v.password.$dirty && !this.$v.password.minLength"
+          class="helper-text invalid"
+          >Password необходимо ввести более
+          {{ this.$v.password.$params.minLength.min }} символов. Введено
+          {{ password.length }}</small
+        >
       </div>
     </div>
     <div class="card-action">
@@ -28,3 +66,45 @@
     </div>
   </form>
 </template>
+
+<script>
+import { email, minLength, required } from "vuelidate/lib/validators";
+
+export default {
+  name: "login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(4) },
+  },
+  computed: {
+    isMailNotValid() {
+      return (
+        (this.$v.email.$dirty && !this.$v.email.required) ||
+        (this.$v.email.$dirty && !this.$v.email.email)
+      );
+    },
+    isPasswordNotValid() {
+      return (
+        (this.$v.password.$dirty && !this.$v.password.required) ||
+        (this.$v.password.$dirty && !this.$v.password.minLength)
+      );
+    },
+  },
+  methods: {
+    submitHandler() {
+      console.log(this.$v);
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      this.$router.push("/");
+    },
+  },
+};
+</script>
